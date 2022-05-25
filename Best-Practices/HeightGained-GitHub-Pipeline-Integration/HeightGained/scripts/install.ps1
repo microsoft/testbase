f54ba85bce073a72bf1a39e81c4d912d0e712b54
Feng Han <fenhan@microsoft.com>
2022-05-25 13:16:21 +0800

    Update install.ps1

--- Best-Practices/HeightGained-GitHub-Pipeline-Integration/HeightGained/scripts/install.ps1
+++ Best-Practices/HeightGained-GitHub-Pipeline-Integration/HeightGained/scripts/install.ps1
@@ -1,1 +1,38 @@
-exit 0
+push-location $PSScriptRoot
+$exit_code = 0
+$script_name = $myinvocation.mycommand.name
+# Root folder
+$root_dir = "$PSScriptRoot\.."
+# Log folder
+$log_dir = "$root_dir\logs"
+$log_file = "$log_dir\$script_name.log"
+Write-Host $log_file
+
+if(-not (test-path -path $log_dir )) {
+    new-item -itemtype directory -path $log_dir
+}
+
+Function log {
+   Param ([string]$log_string)
+   write-host $log_string
+   add-content $log_file -value $log_string
+}
+
+log("Installing .Net 6.0 Runtime")
+$dotnet_root = "$root_dir\.dotnet"
+
+try {
+   Invoke-WebRequest 'https://dot.net/v1/dotnet-install.ps1' -OutFile 'dotnet-install.ps1'
+   ./dotnet-install.ps1 -InstallDir $dotnet_root -Version "6.0.5" -Runtime "dotnet"
+   [System.Environment]::SetEnvironmentVariable("DOTNET_ROOT",$dotnet_root, [System.EnvironmentVariableTarget]::User)
+}
+catch {
+  log("An error occurred:")
+  log($_)
+  log($_.ScriptStackTrace)
+  $exit_code = -1
+}
+log($env:DOTNET_ROOT)
+
+pop-location
+exit $exit_code
