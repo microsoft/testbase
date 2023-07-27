b4c1696b32bcca39840d7f3f77702a79d2ef1d90
Kyle Zhang <v-zhanh@microsoft.com>
2023-07-27 14:30:56 +0800

    update copyright header

--- CustomImage/AzureConfig.ps1
+++ CustomImage/AzureConfig.ps1
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 $logPath = "$PSScriptRoot\AzureConfig.ps1.log"
 
 Start-Transcript -Path $logPath -Append -Force
